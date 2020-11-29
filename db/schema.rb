37c0fa7d9165b8f34bddc186aa6ef3b3cdb414dc
John Tweeddale <j.tweeddale@outlook.com>
2020-11-29 15:15:50 +0000

    books new, create, and index actions, routes, and views completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_143311) do
+ActiveRecord::Schema.define(version: 2020_11_29_143451) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
